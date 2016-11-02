85e05a6507c018ec0507e157432ac8858cd24d06
Nick Reis <email": null>
2016-11-02 09:32:38 -0700

    create pets, toys controller with index and show routes

---
+++ pets_toys/app/controllers/pets_controller.rb
@@ -0,0 +1,9 @@
+class PetsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+end
---
+++ pets_toys/app/controllers/toys_controller.rb
@@ -0,0 +1,8 @@
+class ToysController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+end
