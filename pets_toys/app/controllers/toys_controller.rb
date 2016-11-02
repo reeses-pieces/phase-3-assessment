799d5e0762aebeb0fedea0e9932fa94510037c31
Nick Reis <email": null>
2016-11-02 10:11:32 -0700

    step 7 add shortest toy description for ajax

--- pets_toys/app/controllers/toys_controller.rb
+++ pets_toys/app/controllers/toys_controller.rb
@@ -3,8 +3,11 @@ class ToysController < ApplicationController
   def index
     @pet = Pet.find(params[:pet_id])
     @toys = @pet.toys
+    # Shortest toy description
+    @toys = @pet.toys.sort { |toy1, toy2| toy1.description.length <=> toy2.description.length }
+    @toy = @toys.first
 
-    render json: @toys
+    render json: @toy
 
   end
 
