a51ba83659bc520629e35819b1e539608657f374
Nick Reis <email": null>
2016-11-02 09:45:56 -0700

    step 4 add json response

--- pets_toys/app/controllers/toys_controller.rb
+++ pets_toys/app/controllers/toys_controller.rb
@@ -1,8 +1,17 @@
 class ToysController < ApplicationController
 
   def index
+    @pet = Pet.find(params[:pet_id])
+    @toys = @pet.toys
+
+    if request.xhr?
+      #ajax
+    else
+      render json: @toys
+    end
   end
 
   def show
+    @toy = Toy.find(params[:id])
   end
 end
